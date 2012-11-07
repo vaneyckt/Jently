a3f46c566c57b2cfe44bab46ad3d3f87a2194513
vaneyckt <tom.vaneyck@swrve.com>
2012-11-07 23:04:24 +0000

    fix bug in exception handling

--- jently.rb
+++ jently.rb
@@ -4,36 +4,41 @@ require './lib/jenkins.rb'
 require './lib/helpers.rb'
 
 def test_pull_request(pull_request_id)
-  config = ConfigFile.read
-  pull_request = Github.get_pull_request(pull_request_id)
+  begin
+    config = ConfigFile.read
+    pull_request = Github.get_pull_request(pull_request_id)
 
-  is_test_required = PullRequestsData.is_test_required(pull_request)
-  PullRequestsData.update(pull_request)
+    is_test_required = PullRequestsData.is_test_required(pull_request)
+    PullRequestsData.update(pull_request)
 
-  if is_test_required
-    if pull_request[:mergeable] == false
-      Github.set_pull_request_status(pull_request_id, {:status => 'failure', :description => 'Unmergeable pull request.'})
-    end
-
-    if pull_request[:mergeable] == true
-      Git.clone_repository if !Repository.exists_locally
-      Git.delete_local_testing_branch
-      Git.delete_remote_testing_branch
-      Git.create_local_testing_branch(pull_request)
-      Git.push_local_testing_branch_to_remote
-
-      Jenkins.wait_for_idle_executor
-
-      thr = Thread.new do
-        Github.set_pull_request_status(pull_request_id, {:status => 'pending', :description => 'Started work on pull request.'})
-        job_id = Jenkins.start_job
-        state = Jenkins.wait_on_job(job_id)
-        Github.set_pull_request_status(pull_request_id, state)
+    if is_test_required
+      if pull_request[:mergeable] == false
+        Github.set_pull_request_status(pull_request_id, {:status => 'failure', :description => 'Unmergeable pull request.'})
       end
 
-      timeout = thr.join(config[:jenkins_job_timeout_seconds]).nil?
-      Github.set_pull_request_status(pull_request_id, {:status => 'error', :description => 'Job timed out.'}) if timeout
+      if pull_request[:mergeable] == true
+        Git.clone_repository if !Repository.exists_locally
+        Git.delete_local_testing_branch
+        Git.delete_remote_testing_branch
+        Git.create_local_testing_branch(pull_request)
+        Git.push_local_testing_branch_to_remote
+
+        Jenkins.wait_for_idle_executor
+
+        thr = Thread.new do
+          Github.set_pull_request_status(pull_request_id, {:status => 'pending', :description => 'Started work on pull request.'})
+          job_id = Jenkins.start_job
+          state = Jenkins.wait_on_job(job_id)
+          Github.set_pull_request_status(pull_request_id, state)
+        end
+
+        timeout = thr.join(config[:jenkins_job_timeout_seconds]).nil?
+        Github.set_pull_request_status(pull_request_id, {:status => 'error', :description => 'Job timed out.'}) if timeout
+      end
     end
+  rescue => e
+    Github.set_pull_request_status(pull_request_id, {:status => 'error'})
+    Logger.log('Error when testing pull request', e)
   end
 end
 
@@ -47,7 +52,6 @@ while true
       test_pull_request(pull_request_id)
     end
   rescue => e
-    Github.set_pull_request_status(pull_request_id, {:status => 'error'})
     Logger.log('Error in main loop', e)
   end
   sleep config[:github_polling_interval_seconds]
