7b218bbd3eeb33806776e49bf3a8306f10b33ae9
Tom Van Eyck <tom.vaneyck@swrve.com>
2013-01-27 21:32:25 +0000

    improve logger

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -8,7 +8,7 @@ module Logger
   def Logger.log(message, exception = nil)
     path = get_path
     File.open(path, 'a') { |f| f << "#{Time.now} (#{Time.now.to_i})\n======================================\n#{message} \n\n" } if exception.nil?
-    File.open(path, 'a') { |f| f << "#{Time.now} (#{Time.now.to_i})\n======================================\n#{message} - #{exception.backtrace} \n\n" } if !exception.nil?
+    File.open(path, 'a') { |f| f << "#{Time.now} (#{Time.now.to_i})\n======================================\n#{message} - #{exception} - #{exception.backtrace} \n\n" } if !exception.nil?
   end
 end
 
