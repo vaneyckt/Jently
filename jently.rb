b04d4e6564b5da5f37df18d318d733ec6484694b
Tom Van Eyck <tom.vaneyck@swrve.com>
2013-03-24 17:28:26 +0000

    modify outdated success status detected message

--- jently.rb
+++ jently.rb
@@ -42,7 +42,7 @@ def validate_success_status(pull_request_id)
   begin
     pull_request = Github.get_pull_request(pull_request_id)
     if PullRequestsData.is_success_status_outdated(pull_request)
-      Github.set_pull_request_status(pull_request_id, {:status => 'success', :description => "This will be retested as the #{pull_request[:base_branch]} branch has been updated."})
+      Github.set_pull_request_status(pull_request_id, {:status => 'success', :description => "This has been scheduled for retesting as the '#{pull_request[:base_branch]}' branch has been updated."})
     end
   rescue => e
     Logger.log('Error when validating success status', e)
