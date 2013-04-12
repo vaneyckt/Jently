223f7245bc6826f666374172abacf26af9fe01ba
Adam Alboyadjian <adam@vistahigherlearning.com>
2013-04-12 09:56:15 -0400

    Add specs for Logger, and refactor slightly.

--- lib/helpers/logger.rb
+++ lib/helpers/logger.rb
@@ -1,11 +1,16 @@
 module Logger
+
   def Logger.get_path
     "#{Dir.pwd}/log"
   end
 
   def Logger.log(message, exception = nil)
-    path = get_path
-    File.open(path, 'a') { |f| f << "#{Time.now} (#{Time.now.to_i})\n======================================\n#{message} \n\n" } if exception.nil?
-    File.open(path, 'a') { |f| f << "#{Time.now} (#{Time.now.to_i})\n======================================\n#{message} - #{exception} - #{exception.backtrace} \n\n" } if !exception.nil?
+    exception_output = " - #{exception} - #{exception.backtrace}" if exception
+    File.open(get_path, 'a') { |file| file << log_prefix + message + exception_output.to_s + " \n\n" }
   end
+
+  def self.log_prefix
+    "#{Time.now} (#{Time.now.to_i})\n======================================\n"
+  end
+
 end
--- spec/helpers/logger_spec.rb
+++ spec/helpers/logger_spec.rb
@@ -1,5 +1,40 @@
 require 'spec_helper'
 
 describe Logger do
-  pending "Add some tests"
+  describe '.log' do
+    let(:log_path) { File.join(Dir.pwd, 'rspec_log') }
+
+    before do
+      Logger.stub(:get_path).and_return(log_path)
+      File.unlink(log_path) if File.exists?(log_path)
+    end
+
+    after do
+      File.unlink(log_path) if File.exists?(log_path)
+    end
+
+    context 'when an exception is specified' do
+      let(:exception_message) { 'something bad happened' }
+      let(:exception_backtrace) { 'a valid backtrace' }
+
+      let(:exception) { StandardError.new(exception_message) }
+
+      it 'logs the exception message and backtrace' do
+        exception.stub(:backtrace).and_return(exception_backtrace)
+        Logger.log('anything', exception)
+        results = File.read(log_path)
+        results.should include exception_message
+        results.should include exception_backtrace
+      end
+    end
+
+    context 'when no exception is specified' do
+      it 'logs just the specified log message' do
+        message = "only this should appear"
+        Logger.log(message)
+        results = File.read(log_path)
+        results.should match /.*#{message} \n\n\z/
+      end
+    end
+  end
 end
