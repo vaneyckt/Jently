f31879def38911fdb59ae58e09674c27428c5389
vaneyckt <tom.vaneyck@swrve.com>
2013-06-18 14:44:53 +0100

    use slightly more specific string to split on

--- lib/helpers/repository.rb
+++ lib/helpers/repository.rb
@@ -1,7 +1,7 @@
 module Repository
   def Repository.get_id
     config = ConfigFile.read
-    config[:github_ssh_repository].split('com:').last.split('.git').first
+    config[:github_ssh_repository].split('.com:').last.split('.git').first
   end
 
   def Repository.get_name
