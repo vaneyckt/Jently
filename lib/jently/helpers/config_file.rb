9b298975aa06bf194765c975feb9bc2442c8fc04
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-08-23 23:16:04 +1000

    Use module_function to define class methods. Fix path lookup

--- lib/jently/helpers/config_file.rb
+++ lib/jently/helpers/config_file.rb
@@ -3,7 +3,9 @@ require 'erb'
 require 'yaml'
 
 module ConfigFile
-  def self.read(path)
+  module_function
+
+  def read(path)
     if File.exists?(path)
       raw_data      = IO.read path
       erbified_data = ERB.new(raw_data).result
@@ -13,7 +15,7 @@ module ConfigFile
       config[:whitelist_branches] = (!config.has_key?(:whitelist_branches)) ? Set.new : Set.new(config[:whitelist_branches].compact)
       config
     else
-      Log.log("Config file unavailable -- no log file found in #{get_path}")
+      Log.log("Config file unavailable -- no log file found in #{path}")
     end
   end
 end
