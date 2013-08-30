0f7a5686bb68dd3c54fe5865dca3ac2c4432ee21
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-08-30 23:55:28 +1000

    Use module_function to define class methods in repository helper

--- lib/jently/helpers/repository.rb
+++ lib/jently/helpers/repository.rb
@@ -1,5 +1,6 @@
 module Repository
-  def Repository.get_id
+  module_function
+  def get_id
     config = ConfigFile.read(Jently.config_filename)
     config[:github_ssh_repository].split('.com:').last.split('.git').first
   end
