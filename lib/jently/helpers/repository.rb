aaa06b7634ef0f2308f5c4f5f42d9db177538fcd
Lindsay Holmwood <lindsay@holmwood.id.au>
2013-09-21 17:55:32 +0200

    Add support for https github repositories

--- lib/jently/helpers/repository.rb
+++ lib/jently/helpers/repository.rb
@@ -2,6 +2,12 @@ module Repository
   module_function
   def get_id
     config = ConfigFile.read(Jently.config_filename)
-    config[:github_ssh_repository].split('.com:').last.split('.git').first
+
+    repo = config[:github_ssh_repository]
+    if repo =~ /^http/
+      repo.split('/')[-2..-1].join('/').split('.git').first
+    else
+      repo.split('.com:').last.split('.git').first
+    end
   end
 end
